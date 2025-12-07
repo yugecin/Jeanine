3770450fe4fc23096de5d700c90f669d0bdc8826
yugecin <yugecin@users.noreply.github.com>
2025-12-07 21:07:48 +0100

    wrap Line tests in nested class
    it's been too long so I forgot that I could do this, and this is
    much better

--- src/net/basdon/jeanine/TestLine.java
+++ src/net/basdon/jeanine/TestLine.java
@@ -4,63 +4,66 @@ import static net.basdon.jeanine.TestRunner.*;
 
 public class TestLine
 {
-	private SB sb = new SB(100);
-
-	public void visual_to_logical_pos_around_tab_that_moves_5_chars()
+	public static class VisualToLogicalPos
 	{
-		sb.setLength(0).append("\t---\t---");
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 0));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 1));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 2));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 3));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 4));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 5));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 6));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 7));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 8));
-		assertEquals("bad result", 2, Line.visualToLogicalPos(sb, 9));
-		assertEquals("bad result", 3, Line.visualToLogicalPos(sb, 10));
-		assertEquals("bad result", 4, Line.visualToLogicalPos(sb, 11));
-		assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 12));
-		assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 13));
-		assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 14));
-		assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 15));
-		assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 16));
-		assertEquals("bad result", 6, Line.visualToLogicalPos(sb, 17));
-		assertEquals("bad result", 7, Line.visualToLogicalPos(sb, 18));
-	}
+		private SB sb = new SB(100);
 
-	public void visual_to_logical_pos_when_line_only_has_one_tab()
-	{
-		sb.setLength(0).append("\t");
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 0));
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 1));
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 2));
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 3));
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 4));
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 5));
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 6));
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 7));
-	}
+		public void around_tab_that_moves_5_chars()
+		{
+			sb.setLength(0).append("\t---\t---");
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 0));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 1));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 2));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 3));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 4));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 5));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 6));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 7));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 8));
+			assertEquals("bad result", 2, Line.visualToLogicalPos(sb, 9));
+			assertEquals("bad result", 3, Line.visualToLogicalPos(sb, 10));
+			assertEquals("bad result", 4, Line.visualToLogicalPos(sb, 11));
+			assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 12));
+			assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 13));
+			assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 14));
+			assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 15));
+			assertEquals("bad result", 5, Line.visualToLogicalPos(sb, 16));
+			assertEquals("bad result", 6, Line.visualToLogicalPos(sb, 17));
+			assertEquals("bad result", 7, Line.visualToLogicalPos(sb, 18));
+		}
 
-	public void visual_to_logical_pos_when_line_only_has_two_tabs()
-	{
-		sb.setLength(0).append("\t\t");
-		assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 0));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 1));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 2));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 3));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 4));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 5));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 6));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 7));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 8));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 9));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 10));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 11));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 12));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 13));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 14));
-		assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 15));
+		public void when_line_only_has_one_tab()
+		{
+			sb.setLength(0).append("\t");
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 0));
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 1));
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 2));
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 3));
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 4));
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 5));
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 6));
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 7));
+		}
+
+		public void when_line_only_has_two_tabs()
+		{
+			sb.setLength(0).append("\t\t");
+			assertEquals("bad result", 0, Line.visualToLogicalPos(sb, 0));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 1));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 2));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 3));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 4));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 5));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 6));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 7));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 8));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 9));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 10));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 11));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 12));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 13));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 14));
+			assertEquals("bad result", 1, Line.visualToLogicalPos(sb, 15));
+		}
 	}
 }
